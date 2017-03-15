b8b65734c4abe8163a6a37e4015e62294d205558
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-03-15 11:31:01 -0700

    have left and right working now it is time to get up and down working

---
+++ server.js
@@ -0,0 +1,14 @@
+"use strict";
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.use("/public", express.static("public"));
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, function(){
+    console.log("hello from " + PORT)
+});
